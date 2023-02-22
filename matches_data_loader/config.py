9f0b838ba951b09925ac70fdd61d83d6a834f797
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2023-02-22 18:34:50 +0300

    Increased matches to load
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- matches_data_loader/config.py
+++ matches_data_loader/config.py
@@ -5,7 +5,7 @@ APP_NAME = os.environ['LIQUIPEDIA_APP_NAME']
 DATA_UPDATE_TIMEOUT = 300  # seconds. Actual updates may be slower due to API rate limits (liquipedia mostly)
 STREAM_SEARCH_BEFORE_MATCH_MINUTES = 15  # minutes. Start searching for match streams before match starts
 
-MAXIMUM_MATCHES_TO_LOAD = 5
+MAXIMUM_MATCHES_TO_LOAD = 8
 
 TWITCH_STREAMS_UPDATE_TIMEOUT = 100  # seconds
 TWITCH_THUMBNAIL_EXPIRE = 3600  # seconds. After this time THUMBNAIL will be removed (not to store too many of them)

c995e81688acfc13e126ed20087f4acc6224f356
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2023-02-25 20:57:16 +0300

    increased liquipedia rate limits
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- liquipedia_dota_api/config.py
+++ liquipedia_dota_api/config.py
@@ -1,7 +1,7 @@
 __all__ = ['DEFAULT_PARSE_PERIOD', 'DEFAULT_GET_PERIOD', 'LIQUIPEDIA_URL']
 
 
-DEFAULT_PARSE_PERIOD: float = 30.0  # see https://liquipedia.net/api-terms-of-use for details
-DEFAULT_GET_PERIOD: float = 1.0  # see https://liquipedia.net/api-terms-of-use for details
+DEFAULT_PARSE_PERIOD: float = 45.0  # see https://liquipedia.net/api-terms-of-use for details
+DEFAULT_GET_PERIOD: float = 5.0  # see https://liquipedia.net/api-terms-of-use for details
 
 LIQUIPEDIA_URL: str = 'https://liquipedia.net'
